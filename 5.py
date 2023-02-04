44004d63d2f87842b884be336d1a86ed5e8173cb
maxx-sorokin <maximka773@mail.ru>
2023-02-04 10:21:23 +0300

    5 ok

--- 5.py
+++ 5.py
@@ -3,10 +3,23 @@
 ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 """
 
-x = int(input('Введите число X: '))
-y = int(input('Введите число Y: '))
-z = int(input('Введите число Z: '))
+flag = True
+for x in (0, 1):
+    for y in (0, 1):
+        for z in (0, 1):
+            f = not (x or y or z) == (not x and not y and not z)
+            if f == False:
+                flag = False
+            print(x, y, z, f)
+if flag:
+    print('Доказано')
+else:
+    print('Нет, не во всех случаях')
 
-left = not (x or y or z)
-right = not x and not y and not z
-print(left == right)
+# x = int(input('Введите число X: '))
+# y = int(input('Введите число Y: '))
+# z = int(input('Введите число Z: '))
+
+# left = not (x or y or z)
+# right = not x and not y and not z
+# print(left == right)
